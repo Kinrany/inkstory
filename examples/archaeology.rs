d81b118754d56e0ec653e7936dfa3e431f859ee6
Ruslan Fadeev <kinrany@yandex.com>
2021-04-13 00:37:23 +0300

    Implement Instory format parsing

---
+++ examples/archaeology.rs
@@ -0,0 +1,15 @@
+/// Request and parse a story about archaeology students by Fantom_18.
+use anyhow::Error;
+use fehler::throws;
+use instory::{Diagram, Response as InstoryResponse};
+use ureq;
+
+#[throws()]
+fn main() {
+  let instory: InstoryResponse<Diagram> =
+    ureq::get("https://api.instory.su/api/stories/17801/diagram")
+      .call()?
+      .into_json()?;
+
+  println!("{:?}", instory);
+}
