dc156d52a0e5bcae06ef00673354889f73d3a9f7
Ruslan Fadeev <kinrany@yandex.com>
2021-05-14 23:53:44 +0300

    Move command executor to lib

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,11 @@ pub use ink::Story;
 use ink::{Knot, KnotName};
 pub use instory::{Diagram, Response};
 use instory::{Node, NodeKind};
-use std::error::Error;
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::{error::Error, fs, path::PathBuf, str::FromStr};
+use structopt::StructOpt;
+use url::Url;
 
 fn temp_knot_name(node: &Node) -> KnotName {
   format!("knot_{}", node.id).into()
@@ -48,4 +52,73 @@ pub fn instory_to_ink(diagram: &Diagram) -> Result<Story, Box<dyn Error>> {
     start: start_knot_name,
     knots,
   })
+}
+
+enum StoryLocator {
+  Id(u32),
+  File(PathBuf),
+  Url(Url),
+}
+
+impl StoryLocator {
+  fn get(&self) -> Result<Diagram, Box<dyn Error>> {
+    fn download(url: &Url) -> Result<String, Box<dyn Error>> {
+      Ok(ureq::get(&url.to_string()).call()?.into_string()?)
+    }
+
+    let json = match self {
+      StoryLocator::File(file) => fs::read_to_string(file)?,
+      StoryLocator::Id(id) => download(&instory::diagram_url_from_id(*id))?,
+      StoryLocator::Url(url) => {
+        lazy_static! {
+          static ref RE: Regex =
+            Regex::new(r"^https://instory.su/story/(?P<id>\d+)(/play)?").unwrap();
+        }
+        let url_string = url.clone().to_string();
+        let captures = RE.captures(&url_string);
+        if let Some(captures) = captures {
+          let id = captures.name("id").unwrap().as_str().parse().unwrap();
+          download(&instory::diagram_url_from_id(id))?
+        } else {
+          download(url)?
+        }
+      }
+    };
+    let response: instory::Response<instory::Diagram> = serde_json::from_str(&json)?;
+    Ok(response.data)
+  }
+}
+
+impl FromStr for StoryLocator {
+  type Err = Box<dyn Error>;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    if let Ok(id) = s.parse() {
+      return Ok(StoryLocator::Id(id));
+    }
+
+    if let Ok(url) = s.parse() {
+      return Ok(StoryLocator::Url(url));
+    }
+
+    if let Ok(path) = s.parse() {
+      return Ok(StoryLocator::File(path));
+    }
+
+    Err("Must be a story URL, a file path, or a story ID.".into())
+  }
+}
+
+#[derive(StructOpt)]
+pub struct Inkstory {
+  story_locator: StoryLocator,
+}
+
+impl Inkstory {
+  pub fn exec(&self) -> Result<(), Box<dyn Error>> {
+    let diagram = self.story_locator.get()?;
+    let story = instory_to_ink(&diagram)?;
+    print!("{}", story);
+    Ok(())
+  }
 }
