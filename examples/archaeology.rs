09695698e976437b40a46f656b4d7c29c069eaab
Ruslan Fadeev <github@kinrany.dev>
2025-01-19 19:01:52 +0400

    make a note about example not working

--- examples/archaeology.rs
+++ examples/archaeology.rs
@@ -1,5 +1,6 @@
 //! Request and parse a story about archaeology students by Fantom_18.
 
 fn main() -> anyhow::Result<()> {
+  eprintln!("this example doesn't work");
   inkstory::Inkstory::new("https://api.instory.su/api/stories/17801/diagram")?.exec()
 }
